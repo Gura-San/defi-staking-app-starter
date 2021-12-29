d9c96c2e72113ccb16c07d0e62604b24db5f0159
David Guras <gura.san@gmail.com>
2021-12-29 08:32:03 -0500

    Import Tether and RWD tokens to DecentralBank contract

--- defi-staking-app-starter/src/contracts/DecentralBank.sol
+++ defi-staking-app-starter/src/contracts/DecentralBank.sol
@@ -2,7 +2,17 @@
 
 pragma solidity ^0.5.0;
 
+import "./RWD";
+import "./Tether";
+
 contract DecentralBank {
     string public name = "Decentral Bank";
     address public owner;
+    Tether public tether;
+    RWD public rwd;
+
+    constructor(RWD _rwd, Tether _tether) {
+        rwd = _rwd;
+        tether = _tether;
+    }
 }
