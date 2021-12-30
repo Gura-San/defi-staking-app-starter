8d28cc672f8e0dc57455683cd1edce137d3f7638
David Guras <gura.san@gmail.com>
2021-12-29 19:11:44 -0500

    Tweak tokenomics
    - add code to transfer all RWD tokens to Decentral Bank
    - add code to distribute 100 tether tokens to investor

--- defi-staking-app-starter/migrations/2_deploy_contracts.js
+++ defi-staking-app-starter/migrations/2_deploy_contracts.js
@@ -2,13 +2,22 @@ const Tether = artifacts.require("Tether");
 const RWD = artifacts.require("RWD");
 const DecentralBank = artifacts.require("DecentralBank");
 
-module.exports = async function (deployer) {
+module.exports = async function (deployer, network, accounts) {
   // Deploy Mock Tether Contract
   await deployer.deploy(Tether);
+  const tehter = await Tether.deployed();
 
   // Deploy Reward  Contract
   await deployer.deploy(RWD);
+  const rwd = await RWD.deployed();
 
   // Deploy DecentralBank Contract
   await deployer.deploy(DecentralBank);
+  const decentralBank = await DecentralBank.deployed();
+
+  // Transfer all RWD tokens to Decentral Bank
+  await rwd.transfer(DecentralBank.address, "1000000000000000000000000");
+
+  // Distribute 100 tether tokens to investor
+  await tether.transfer(accounts[1], "1000000000000000000");
 };
