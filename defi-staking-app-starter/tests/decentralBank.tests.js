37581b8220e617665d943d26b5a4ddd1d82ca271
David Guras <gura.san@gmail.com>
2021-12-29 22:09:48 -0500

    Test Mock Tether Deployment
    - add "it matches name successfully" test

--- defi-staking-app-starter/tests/decentralBank.tests.js
+++
@@ -1,9 +0,0 @@
-const Tether = artifacts.require("Tether");
-const RWD = artifacts.require("RWD");
-const DecentralBank = artifacts.require("DecentralBank");
-
-require("chai").use(require("chai-as-promised")).should();
-
-contract("DecentralBank", (accounts) => {
-  // Tests go here
-});
