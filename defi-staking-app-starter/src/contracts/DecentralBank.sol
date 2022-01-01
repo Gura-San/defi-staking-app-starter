24d5185b1ce8f9273a21681a09d26f60fd953646
David Guras <gura.san@gmail.com>
2022-01-01 14:49:04 -0500

    Add staking functionality to Test Decentral Bank

--- defi-staking-app-starter/src/contracts/DecentralBank.sol
+++ defi-staking-app-starter/src/contracts/DecentralBank.sol
@@ -11,8 +11,34 @@ contract DecentralBank {
     Tether public tether;
     RWD public rwd;
 
+    address[] public stakers;
+
+    mapping(address => uint) public stakingBalance;
+    mapping(address => bool) public hasStaked;
+    mapping(address => bool) public isStaking;
+
     constructor(RWD _rwd, Tether _tether) public {
         rwd = _rwd;
         tether = _tether;
+    }
+
+    // Staking function
+    function depositTokens(uint _amount) public {
+        // Require staking amount to be greater than 0
+        require(_amount > 0, "Amount has to be positive");
+
+        // Transfer tether tokesn to this contract address for staking
+        tether.transferFrom(msg.sender, address(this), _amount);
+
+        // Update Staking Balance
+        stakingBalance[msg.sender] += _amount;
+
+        if(!hasStaked) {
+            stakers.push[msg.sender];
+        }
+
+        // Update Staking Balance
+        isStaking[msg.sender] = true;
+        hasStaked[msg.sender] = true;
     }
 }
